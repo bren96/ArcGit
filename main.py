dfd1291a225835c8285e1956a05ebb227d942430
Brendan Cullen <brendancullen996@gmail.com>
2021-08-18 22:22:39 -0400

    update gitignore, readme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arcgit
 
 if __name__ == "__main__":
+    pass
     # arcgit.clone_dashboard('c99d39d96af848bb802ff29f50cd6017')
-    arcgit.merge('Dashboard_9a80a2dc30d849a58dc376c95347ea8c')
+    # arcgit.merge('Dashboard_9a80a2dc30d849a58dc376c95347ea8c')
 
