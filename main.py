c9010f4ab8ed4a2e0fbb4404abd1f5243116fbe6
Brendan Cullen <brendancullen996@gmail.com>
2021-08-20 00:08:28 -0400

    update readme, add notes

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import arcgit
 
 if __name__ == "__main__":
     pass
-    # arcgit.clone_dashboard('c99d39d96af848bb802ff29f50cd6017')
+    arcgit.clone_dashboard('c99d39d96af848bb802ff29f50cd6017')
     # arcgit.merge('Dashboard_9a80a2dc30d849a58dc376c95347ea8c')
 
